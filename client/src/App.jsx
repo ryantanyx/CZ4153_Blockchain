e5bb4b2a73ac5fa6c89207debcdf4f1a126d58f3
Amadeus Koh <60766014+amadeuskyj@users.noreply.github.com>
2022-10-25 16:21:22 +0800

    Add creategameform

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,18 @@ import Setup from "./components/Setup";
 import Demo from "./components/Demo";
 import Footer from "./components/Footer";
 import "./App.css";
-import Navbar from "./components/Navbar"
-import { Container, Box, Typography, Grid, CardMedia, Button } from '@mui/material';
+import Navbar from "./components/Navbar";
+import CreateGameForm from "./components/CreateGameForm";
+import { Container, Box, Typography, Grid, CardMedia, Button, Card, CardContent, Dialog, IconButton } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 import banner from "./banner.jpg";
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const openCreateGameForm = () => {
+    setOpen(true);
+  }
 
   return (
     <Box>
@@ -28,7 +35,7 @@ function App() {
                   Know something others don't? Why not profit from it? Make your bets now!
                 </Typography>
               </Box>
-              <Button variant="contained" color="success" sx={{ my: 2 }}>
+              <Button variant="contained" color="success" sx={{ my: 2, fontWeight: 700 }} endIcon={<SendIcon />} onClick={openCreateGameForm} >
                 Or Click Here to Create Your Own Prediction Game
               </Button>
             </Grid>
@@ -43,8 +50,43 @@ function App() {
           </Grid>
         </Container>
       </Box>
-      <Box>
+      <Dialog open={open} fullWidth={true} maxWidth="md">
+        <CreateGameForm onCloseForm={setOpen} />
+      </Dialog>
+      <Box sx={{ my: 10 }}>
+        <Container fixed>
+          <Grid container spacing={2}>
+            <Grid xs={3}>
+              <Card>
+                <CardContent>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    benovelent
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid xs={3}>
 
+            </Grid>
+            <Grid xs={3}>
+
+            </Grid>
+            <Grid xs={3}>
+
+            </Grid>
+          </Grid>
+        </Container>
       </Box>
     </Box>
   );
